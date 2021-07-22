56e117f4b0bac9b9f2a36d7808e18379b306f6d8
Ahmed Mulla <ahmedmulla901@gmail.com>
2021-07-22 23:25:12 +0530

    initial commit

---
+++ Register/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout
+from .forms import RegistrationForm
+
+# Create your views here.
+
+def register(response):
+    if response.method == 'POST':
+        form=RegistrationForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=RegistrationForm()
+    return render(response,"Register/register.html",{"form":form})
+
+
+
+
---
+++ Teacher/urls.py
@@ -0,0 +1,12 @@
+from django.urls import  path
+from . import views
+
+urlpatterns = [
+    path("",views.home,name="Home"),
+    path("<int:id>",views.index,name="index"),
+    path("announcement/",views.create_announcement,name="Announcement"),
+    path("class/",views.create_class,name="Extra Class"),
+    path("exam/",views.create_exam,name="Exams"),
+    path("logout/",views.logout_request,name="logout"),
+    
+]
